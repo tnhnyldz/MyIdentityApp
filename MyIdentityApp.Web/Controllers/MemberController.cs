541589ad7c81db11d1e9d354246e6985b6e10197
Tnhnyildiz <tunahanyildiz@outlook.it>
2024-03-19 00:14:25 +0300

    logout and cookie options completed

---
+++ MyIdentityApp.Web/Controllers/MemberController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyIdentityApp.Web.Models;
+
+namespace MyIdentityApp.Web.Controllers
+{
+	public class MemberController : Controller
+	{
+		private readonly SignInManager<AppUser> _signInManager;
+		private readonly ILogger<MemberController> _logger;
+
+		public MemberController(SignInManager<AppUser> signInManager, ILogger<MemberController> logger)
+		{
+			_signInManager = signInManager;
+			_logger = logger;
+		}
+
+		public async Task LogOut()
+		{
+			await _signInManager.SignOutAsync();
+		}
+	}
+}
